a102e2759fcfe8cc195d6b7aa5169ab3be53c7df
Lovnyth Valcin <lovnyth@gmail.com>
2025-02-28 22:42:34 +0000

    button added to card

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -14,6 +14,7 @@ const Card = ({propTitle}) => {
         <h5 className="card-title">{propTitle}</h5>
         <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <button type="button" class="btn btn-primary">Explore Packages</button>
       </div>
     </div>
 
