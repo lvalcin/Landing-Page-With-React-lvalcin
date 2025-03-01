0b9e12df04486b3802b0bdf7d0c5a4a856e87243
Lovnyth Valcin <lovnyth@gmail.com>
2025-03-01 02:30:09 +0000

    card updated by mapping object array

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -4,16 +4,18 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Card = ({propTitle}) => {
+const Card = ({cardProp}) => {
+  console.log(cardProp.title, "here is the title")
 	return (
-   <div className= "text-center">
+   <div className= "container">
     
 
     <div className="card rounded-1 d-flex" style= {{ width: "16rem" }}>
       <div className="card-body">
-        <h5 className="card-title">{propTitle}</h5>
-        <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <img src={cardProp.img} class="card-img-top" alt="..."/>
+        <h5 className="card-title">{cardProp.title}</h5>
+        <h6 className="card-subtitle mb-2 text-body-secondary">cardProp.paragrph</h6>
+        <p className="card-text">cardProp.paragrph</p>
         <button type="button" class="btn btn-primary">Explore Packages</button>
       </div>
     </div>
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,20 +7,39 @@ import Title from "./Title";
 import Card from "./Card";
 
 //create your first component
-const cardTitle=["Carribean/Latin America", "Africa", "Asia/Middle East", "Europe"]
 
 
+const cardObj=[
+		{title:"Carribean/Latin America",
+		img: "https://images.unsplash.com/photo-1638832406094-b7c061f7e02e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		paragraph: "Here is some info",
+		},
+		{title:"Africa",
+		img: "https://images.unsplash.com/photo-1611693196679-c1f358840cc6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		paragraph: "Here is some info",
+		},
+		{title:"Asia/Middle East",
+		img: "https://images.unsplash.com/photo-1546412414-8035e1776c9a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		paragraph: "Here is some info",
+		},
+		{title:"Europe",
+		img: "https://images.unsplash.com/photo-1546610072-90a8cdd6aa4d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		paragraph: "Here is some info",
+		}
+
+]
+	
 
 const Home = () => {
 	return (
 		<div className="text-center">
             <Navbar />
 			<Title />
-			{cardTitle.map(
+			{cardObj.map(
 				(item)=>{
 					return(
 						<div>
-							<Card propTitle={item} />
+							<Card cardProp ={item} />
 						</div>
 					)	
 				}
