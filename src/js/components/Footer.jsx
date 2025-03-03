db507f69f2ae1b623739c5dc432e7fcd2049a3c3
Lovnyth Valcin <lovnyth@gmail.com>
2025-03-03 01:18:29 +0000

    navbar edited with heading on left side

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const Footer = () => {
 	return (
 		<div className="text-center">
-    		<footer class="bg-danger-subtle text-center py-3 mt-auto">
+    		<footer class="bg-danger-subtle text-center py-3 mt-4">
         		<p class="mb-0">&copy; 2025. All Rights Reserved.</p>
     		</footer>
 		</div>
--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -7,20 +7,23 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Navbar = () => {
     return (
         <div className="text-center">
-            <ul class="nav justify-content-end bg-danger-subtle mb-4">
-  <li class="nav-item">
-    <a class="nav-link" href="#">Home</a>
-  </li>
-  <li class="nav-item ">
-    <a class="nav-link " href="#">Services</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">About Us</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#">Contact US</a>
-  </li>
-</ul>
+      <ul class="nav bg-danger-subtle mb-4">
+          <li class="nav-item h5 ms-5">
+            <a class="nav-link text-dark-emphasis" href="#">Your Next Getaway</a>
+          </li>
+          <li class="nav-item ms-auto">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link " href="#">Services</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="#">About Us</a>
+          </li>
+          <li class="nav-item me-5">
+            <a class="nav-link disabled " href="#">Contact US</a>
+          </li>
+      </ul>
 
         </div>
     );
