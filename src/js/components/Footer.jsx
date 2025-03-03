ec96a1532894920a802c05bbdf30c722274cb227
Lovnyth Valcin <lovnyth@gmail.com>
2025-03-03 00:11:40 +0000

    footer added to page

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -5,7 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const Footer = () => {
 	return (
 		<div className="text-center">
-            This is my footer
+    		<footer class="bg-danger-subtle text-center py-3 mt-auto">
+        		<p class="mb-0">&copy; 2025. All Rights Reserved.</p>
+    		</footer>
 
 		</div>
 	);
